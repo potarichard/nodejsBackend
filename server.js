fce0a199834ae84b8d24925b613e116f34dbd296
myname <richard.i.pota@gmail.com>
2020-08-31 12:22:12 +0200

    authentication, and eureka client in progress

--- server.js
+++ server.js
@@ -5,15 +5,12 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')                              // @RequestBody, ahhoz kell hogy server tudjon fogadni endpontjain adott pojot. post, vagy put ban
-const methodOverride = require('method-override')
 
 const indexRouter = require('./routes/index')
 const authorRouter = require('./routes/authors')
 const bookRouter = require('./routes/books')
 
 const app = express()
-// app.use(methodOverride('_method'))                                      // a formokrol majd "_method" elojelzessel kell jelezni hogy PUT, DELETE request.
-// app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))      // use bodyparser, + multipart file korlatozas.
 app.use(bodyParser.json());                                                // use json body parser !!!
 
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
